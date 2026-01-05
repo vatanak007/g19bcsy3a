10295f48c4f29f997cd434c5315d5b6eb21bc6f2
vatanak007 <vatanak080@gmail.com>
2026-01-05 10:30:14 +0700

    week 1

---
+++ pages/auth/register.php
@@ -0,0 +1,37 @@
+<?php
+include '../../includes/header.inc.php';
+include '../../includes/navbar.inc.php';
+?>
+
+<body>
+
+
+
+
+    <form class="col-md-10 col-lg-6 mx-auto">
+        <h3>Register Page</h3>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+            <input type="comfirmpassword" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+
+    </form>
+
+    <?php
+    include '../../includes/footer.inc.php'
+    ?>
